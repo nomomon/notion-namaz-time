0c5746e64ab86133af1303a27cceea4132ff7049
nomomon <m.nurmukhambetov@researchable.nl>
2023-02-17 14:56:00 +0100

    updateBasePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    basePath: '/notion-namaz-time'
 }
 
 module.exports = nextConfig
